b17dc367c820ce2a916e1dadf2a1ec92a722a613
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-08-17 23:48:17 -0400

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+const path = require('path');
+
+// Connect to database
+connectDB();
+
+app.use(express.json());
+
+// Define Routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('/ittybittyurl/build'));
+    app.get('*', (req, res) => {  
+
+        res.sendFile(path.resolve(__dirname, 'ittybittyurl', 'build', 'index.html'));
+
+    });
+    console.log('Serving React');
+};
+
+//const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
